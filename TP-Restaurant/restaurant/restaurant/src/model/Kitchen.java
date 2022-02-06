c7095f2f2b5c6c3ace6a0bf26adfbc86a1be1336
Mysterikkrimkss <krimkss@gmail.com>
2022-02-06 19:31:27 +0100

    socle v3
    finalisation de la base du projet

--- TP-Restaurant/restaurant/restaurant/src/model/Kitchen.java
+++ TP-Restaurant/restaurant/restaurant/src/model/Kitchen.java
@@ -10,4 +10,14 @@ public class Kitchen {
 	public void add(Order order) {
 		tasksToDo.add(order);
 	}
+	
+	public void prepareDish(Server server, Table table, Dish dish) {
+		if(dish.decrementStockAmount()) {
+			ringServer(server, table, dish);
+		}
+	}
+	
+	public void ringServer(Server server, Table table, Dish dish) {
+		server.serveDish(table, dish);
+	}
 }
