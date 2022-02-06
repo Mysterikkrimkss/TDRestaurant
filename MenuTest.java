b967e9a41b86db8ecc1f05f94d02d0229f580a10
Faustinthebuilder <49815560+Faustinthebuilder@users.noreply.github.com>
2022-02-06 20:52:18 +0100

    Test des menus
    test des menus, sans avoir utilisé Junit

---
+++ MenuTest.java
@@ -0,0 +1,13 @@
+package model;
+import junit.framework.*;
+public class MenuTest {
+
+	@test
+	public void testDishes() throws Exception
+	{
+		Menu m = new Menu(m);
+		Dish d = new Dish();
+		AssertEquals(m.setDishPrice(d, d.getPrice()));
+		
+	}
+}
