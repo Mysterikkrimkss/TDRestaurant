f843d40991a5eaf14b0b3f542dc8b110c5de11e2
mostafaza <kic.mostafa@gmail.com>
2022-02-06 18:39:08 +0100

    test menus

---
+++ TP-Restaurant/demo/demo/src/test/java/com/example/restaurant/TestMostafa.java
@@ -0,0 +1,32 @@
+package com.example.restaurant;
+
+import org.junit.jupiter.api.Test;
+
+public class priceTests {
+
+ @Test
+ @ValueSource(doubles = {20.0, 15.0})
+  public void VerifyThatPriceAreEqual(Double price) throws Exception {
+
+    Franchise franchise = new Franchise();
+    Restaurant resto = new Restaurant();
+
+    franchise.setDishPrice(price);
+
+    assertEquals(resto.getPrice(), price);
+  }
+
+ @Test
+ @ValueSource(doubles = {20.0, 15.0})
+  public void VerifyThatPriceAreNotEqual(Double price) throws Exception {
+
+    Restaurant resto = new Restaurant();
+    resto.setDishPrice(price);
+ 
+    Franchise franchise = new Franchise();
+  
+	assertNotEquals(franchise.getPrice(), price);
+  }
+} 
+
+    
